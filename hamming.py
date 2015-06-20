7055709f5faf7e691189350a40b4ffbb38549a14
Isaqb <isaqbarbosa@gmail.com>
2015-06-20 17:48:10 -0300

    Update hamming.py

--- hamming.py
+++ hamming.py
@@ -1,3 +1,37 @@
-def hamming_distance(s1, s2):
-    assert len(s1) == len(s2)
-    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+def paridade (mensagem) :
+	mensagem = list(mensagem)
+	for i in range(0, len(mensagem)):
+		posicao = 2** i
+		if posicao < len(mensagem):
+			mensagem.insert(posicao-1, "*")
+		else:
+			break
+	mensagem = "".join(mensagem)		
+	return mensagem	
+def calcularparidade(cadeia):
+	aux = cadeia
+	x = cadeia.count("*")
+	i=0
+	while x > i :
+		salto = 2**i
+
+		while len(cadeia)>0:
+			
+		i+=1
+
+	return 		
+def erros(cadeia):
+	for contendo in cadeia.items():
+		soma=0
+		for elemento in contendo:
+			for caracter in elemento:
+				if caracter != "*":
+					soma += int(caracter)
+	return cadeia				
+def main():
+	mensagem = raw_input()
+	cadeia1=paridade(mensagem)
+	cadeia2=calcularparidade(cadeia1)	
+	cadeia = erros(cadeia2)
+	print "Erros:\n", cadeia
+main()
